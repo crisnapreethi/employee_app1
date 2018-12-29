587a5497c5063a512eb1358154b7491707c36e42
CrisnaPreethi <crisnapreethi1992@gmail.com>
2018-12-29 17:51:50 +0530

    First Page Commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def hello
+    render html: "Please add '/employees' to the link above to proceed to the app!"
+  end
 end
